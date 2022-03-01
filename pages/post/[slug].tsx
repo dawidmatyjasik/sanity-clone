2e1057816aadb1e052b3cc2df37a05b2fac2fed1
Dawid Spisak <dawidspisak@gmail.com>
2022-03-01 21:11:24 +0100

    complete comment section

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -163,6 +163,19 @@ const Post = ({ post }: Props) => {
           />
         </form>
       )}
+      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              {' '}
+              <span className="text-yellow-500">{comment.name}:</span>{' '}
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
